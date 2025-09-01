65c48f2fef9abdd249d5f4b2640041c850fae768
spooky5880 <entomb7955@outlook.com>
2025-09-02 01:23:19 +0400

    Upload Files

---
+++ Crypter v2.0/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Crypter")> 
+<Assembly: AssemblyDescription("Crypter v2.0 By Unknown")> 
+<Assembly: AssemblyCompany("by unknown")> 
+<Assembly: AssemblyProduct("Crypter")> 
+<Assembly: AssemblyCopyright("Copyright © unknown 2024")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("66811ef9-7ce4-4983-b0e3-1424c24a23dc")> 
+
+
+<Assembly: AssemblyVersion("2.0.0.0")> 
+<Assembly: AssemblyFileVersion("2.0.0.0")> 
