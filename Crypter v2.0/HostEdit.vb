63c4b24590ebde677b52d50f6a9f8871c2b22020
spooky5880 <entomb7955@outlook.com>
2025-09-02 01:22:57 +0400

    Upload Files

---
+++ Crypter v2.0/HostEdit.vb
@@ -0,0 +1,14 @@
+﻿Public Class HostEdit
+
+    Private Sub BullionButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BullionButton1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Bullion_Click(sender As Object, e As EventArgs) Handles Bullion.Click
+
+    End Sub
+End Class
